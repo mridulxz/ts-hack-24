3782f877ab93b380cc38d034b10a338886de6c1b
rexdotsh <65942753+rex69420@users.noreply.github.com>
2024-11-21 04:23:52 +0530

    feat: load environment variables and simplify template rendering; add 404 error handler

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 
+from dotenv import load_dotenv
 from flask import Flask, redirect, render_template, url_for
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "dev")
@@ -8,21 +11,26 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "dev")
 
 @app.route("/")
 def home():
-    return render_template("index.html", active_page="home")
+    return render_template("index.html")
 
 
 @app.route("/shop")
 def shop():
-    return render_template("shop.html", active_page="shop")
+    return render_template("shop.html")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html", active_page="careers")
+    return render_template("careers.html")
 
 
 @app.route("/about")
 def about():
+    return redirect(url_for("home"))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
     return redirect(url_for("home"))
 
 
